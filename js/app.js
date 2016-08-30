1370e6a6b7057a38ecb48be184020e9fab44df1f
gerardolsj <lsjgerardo@gmail.com>
2016-08-30 14:55:07 -0500

    camera working

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var cimesa = angular.module('cimesa', ['ngRoute','ui.bootstrap','ionic']);
+var cimesa = angular.module('cimesa', ['ngRoute','ionic']);
 
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
@@ -39,6 +39,12 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        pictureSource=navigator.camera.PictureSourceType;
+        destinationType=navigator.camera.DestinationType;
+        checkConnection(); /**Check conection app = this */
+
+
+
                     /** */
     },
     // Update DOM on a Received Event
@@ -47,13 +53,125 @@ var app = {
                 /**/
     },
 
-    onErr: function(error)
-    {
+    onErr: function(error){
         alert('Unable to get your location. Without location you will not be able to use navigate feature! Error:' + '\n' + error.message);
     },
 
-    onSuccess: function(position)
-    {
+    onSuccess: function(position){
                 /** */
     }
 };
+
+
+
+    function checkConnection(){
+    var networkState = navigator.connection.type;
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    console.warn('Connection type: ' + states[networkState]);
+
+
+    }
+
+    function onPhotoDataSuccess(imageData) {
+      // Get image handle
+      //
+  
+      console.log("onPhotoDataSuccess: "+imageData);
+
+      $('#gallery').append($('<img>',{src:"data:image/jpeg;  base64," + imageData}));
+/*
+      var smallImage = document.getElementById('smallImage');
+      // Unhide image elements
+      //
+      smallImage.style.display = 'block';
+      // Show the captured photo
+      // The inline CSS rules are used to resize the image
+      //
+      smallImage.src = "data:image/jpeg;  base64," + imageData;
+      */
+    }
+    
+	// Called when a photo is successfully retrieved
+    //
+    function onPhotoFileSuccess(imageData) {
+      // Get image handle
+      console.log(JSON.stringify(imageData));
+      console.log("onPhotoFileSuccess: "+imageData);
+      $('#gallery').append($('<img>',{src: imageData}));
+
+      /*
+   	  // Get image handle
+      //
+      var smallImage = document.getElementById('smallImage');
+      // Unhide image elements
+      //
+      smallImage.style.display = 'block';
+      // Show the captured photo
+      // The inline CSS rules are used to resize the image
+      //
+      smallImage.src = imageData;
+      */
+    }
+    // Called when a photo is successfully retrieved
+    //
+    function onPhotoURISuccess(imageURI) {
+      // Uncomment to view the image file URI 
+      // console.log(imageURI);
+      // Get image handle
+      //
+            console.log("onPhotoURISuccess"+imageURI);
+      $('#gallery').append($('<img>',{src: imageURI}));
+      /*
+      var largeImage = document.getElementById('largeImage');
+      // Unhide image elements
+      //
+      largeImage.style.display = 'block';
+      // Show the captured photo
+      // The inline CSS rules are used to resize the image
+      //
+
+      $("#gallery").append("URRRIIII");
+      largeImage.src = imageURI;
+      */
+    }
+    // A button will call this function
+    //
+    function capturePhotoWithData() {
+      // Take picture using device camera and retrieve image as base64-encoded string
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50 });
+    }
+
+
+
+    function capturePhotoWithFile() {
+        navigator.camera.getPicture(onPhotoFileSuccess, onFail, { 
+            quality: 50, 
+            destinationType: Camera.DestinationType.FILE_URI,
+             saveToPhotoAlbum: true
+        });
+    }
+    
+    // A button will call this function
+    //
+    function getPhoto(source) {
+      // Retrieve image file location from specified source
+      navigator.camera.getPicture(onPhotoURISuccess, onFail, { 
+        quality: 50, 
+        destinationType: destinationType.FILE_URI,
+        sourceType: source
+     });
+    }
+    // Called if something bad happens.
+    // 
+    function onFail(message) {
+      alert('Failed because: ' + message);
+    }
